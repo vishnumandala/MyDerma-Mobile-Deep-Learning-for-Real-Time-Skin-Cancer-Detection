6494ce189cd54b22ffaad2b858a07906f7e8dbbd
Aashrita Chemakura <achemaku@umd.edu>
2024-06-23 18:09:37 -0400

    deleted app code

--- App/android/app/src/main/kotlin/com/google/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.google.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- App/lib/utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const gradient = BoxDecoration(
-        gradient: LinearGradient(
-        begin: Alignment.centerLeft,
-        end: Alignment.centerRight,
-        colors: [Color(0xff3EEBB4), Color(0xff30AFE4)]));
-
-const USER_IMAGE='assets/images/Derman.png';
